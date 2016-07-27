2482dce4969365ff23a6db15de4b7f68186fcb0d
Matan Lachmish <matan.lachmish@jivesoftware.com>
2016-07-27 18:53:40 +0300

    Organize test files

---
+++ CryptographyTests/Algorithms/MAC/HMACTests.swift
@@ -0,0 +1,29 @@
+//
+//  HMACTests.swift
+//  Cryptography
+//
+//  Created by Matan Lachmish on 27/07/2016.
+//  Copyright © 2016 The Big Fat Ninja. All rights reserved.
+//
+
+import XCTest
+
+class HMACTests: XCTestCase {
+
+    override func setUp() {
+        super.setUp()
+    }
+    
+    override func tearDown() {
+        super.tearDown()
+    }
+
+    func testExample() {
+    }
+
+    func testPerformanceExample() {
+        self.measureBlock {
+        }
+    }
+
+}
